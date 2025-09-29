d043b8734757aeab8d3673e20756d04552a8cf4a
Anahita Sharma <sharma.anahita.as@gmail.com>
2025-09-29 17:33:26 +0530

    learnt more about mongoose

---
+++ lec-5/models/todos/sub_todo.models.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+//make schema
+
+const userSchema = new mongoose.Schema({
+    username :{
+        type : String,
+        required :true,
+        unique : true
+    },
+    email : {
+        type: String,
+        required : [true,"email is to be given"],
+        unique: true,
+        lowercase : true
+    }
+        ,
+        password:{
+            type : String,
+            required : [true,"password is required"],
+            min : [8, "password must be atleast 8 characters"]
+        },
+    isActive : Boolean,
+    contact_email : String ,
+    contactSameEmail : Boolean
+})
+
+//export this schema or this won't be make
+
+export const User = mongoose.model("User",userSchema) // kya model banana h aur kiske basis pe banana h
